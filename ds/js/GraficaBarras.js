6fea2a1283f201d83e15bb9d623ed5badd00400f
Diego Garces <thebroyt.290@gmail.com>
2024-09-09 14:11:35 -0600

    Set data

--- ds/js/GraficaBarras.js
+++ ds/js/GraficaBarras.js
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var options = {
         series: [{
             name: 'Total',
-            data: [78, 6]
+            data: [32, 7]
         }],
         chart: {
             toolbar: {
--- ds/js/GraficaDona.js
+++ ds/js/GraficaDona.js
@@ -1,4 +1,4 @@
-var rendimiento = "92%";
+var rendimiento = "82%";
 
 // Función para actualizar el tamaño del gráfico y la fuente
 function updateChartSize() {
@@ -49,7 +49,7 @@ function updateChartSize() {
 }
 
 var options = {
-    series: [78, 6],
+    series: [32, 7],
     chart: {
         width: 500,
         type: 'donut',
