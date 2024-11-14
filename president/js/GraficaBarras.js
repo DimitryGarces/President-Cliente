b7a2550ce895d5d50789f62cc3acf433eecdfa78
Diego Garces <thebroyt.290@gmail.com>
2024-11-14 13:54:34 -0600

    Update

--- president/js/GraficaBarras.js
+++ president/js/GraficaBarras.js
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var options = {
         series: [{
             name: 'Total',
-            data: [1760, 139]
+            data: [1789, 119]
         }],
         chart: {
             toolbar: {
--- president/js/GraficaDona.js
+++ president/js/GraficaDona.js
@@ -49,7 +49,7 @@ function updateChartSize() {
 }
 
 var options = {
-    series: [1760, 139],
+    series: [1789, 119],
     chart: {
         width: 500,
         type: 'donut',
